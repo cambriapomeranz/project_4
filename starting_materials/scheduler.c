a4a357de4c982696149e5cf1b59de7f2d14bef69
jjmurphy1114 <jjmurphy1114@outlook.com>
2023-10-06 19:43:56 +0000

    balls

--- starting_materials/scheduler.c
+++ starting_materials/scheduler.c
@@ -72,18 +72,20 @@ void  RR(struct Job* head, int time_slice) {
     printf("Peforming the RR policy:\n");
     struct Job* current_job = head;
     int jobs_left = 1;
-    while(jobs_left) {
+    while(jobs_left == 1) {
         jobs_left = 0;
+        current_job = head;
         while (current_job != NULL) {
             if (current_job->length > time_slice) {
                 jobs_left = 1;
                 printf("Job %d ran for: %d\n", current_job->id, time_slice);
                 current_job->length -= time_slice;
             }
-            else if (current_job->length != 0) {
+            else if (current_job->length > 0) {
                 printf("Job %d ran for: %d\n", current_job->id, current_job->length);
+                current_job->length = 0;
             }
-        current_job = current_job->next;
+            current_job = current_job->next;
         }
     }
 }
@@ -139,7 +141,11 @@ int main(int argc, char *argv[]){
     } else if (strcmp(argv[1], "SJF") == 0){
         SJF(head);
     } else if (strcmp(argv[1], "RR") == 0){
-        RR(head, atoi(argv[4]));
+        if(atoi(argv[4]) == 0){
+            printf("Time slice must be greater than 0\n");
+        } else{
+            RR(head, atoi(argv[4]));
+        }
     } else{
         printf("The policy you inputted is not correct\n");
     }
